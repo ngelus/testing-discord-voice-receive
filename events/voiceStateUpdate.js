9fa588bdec8861f71a5db1a1e17a9da3b57c24b2
Naggelus <25223259+ngelus@users.noreply.github.com>
2023-05-24 21:07:20 +0200

    Implemented continuous voice channel following, so that I don't always have to summon the bot. Just for faster debugging and iteration.

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,9 +1,13 @@
 const { Events, VoiceState } = require('discord.js');
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
+require('dotenv').config();
+const { DISCORD_OWNER_ID: OWNER_ID } = process.env;
 
 module.exports = {
   name: Events.VoiceStateUpdate,
   once: false,
   async execute(oldState, newState) {
+    var vc = '';
     if (oldState.channel == newState.channel) {
       /**
        * When a user deafens or mutes themselfs.
@@ -14,13 +18,37 @@ module.exports = {
       /**
        * When a user joins a voice channel.
        */
-      return console.log('USER JOINED A VC');
+      if (newState.member.id == OWNER_ID) {
+        try {
+          vc = joinVoiceChannel({
+            channelId: newState.member.voice.channelId,
+            guildId: newState.guild.id,
+            adapterCreator: newState.guild.voiceAdapterCreator,
+            selfDeaf: false,
+          });
+        } catch (error) {
+          console.error(
+            `[ERROR] Error joining ${newState.member.voice.channel?.name}!`
+          );
+          console.error(error);
+        }
+        return;
+      }
     }
-    if (oldState.channel) {
+    if (oldState.channel && !newState.channel) {
       /**
        * When a user leaves a voice channel.
        */
-      return console.log('USER LEFT A VC');
+      try {
+        vc = getVoiceConnection(oldState.guild.id);
+        vc.disconnect();
+      } catch (error) {
+        console.error(
+          `[ERROR] Error disconnecting from ${newState.member.voice.channel?.name}!`
+        );
+        console.error(error);
+      }
+      return;
     }
   },
 };
