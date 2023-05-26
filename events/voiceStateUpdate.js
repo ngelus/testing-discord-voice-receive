67de65917617a76acef62b9053dc02a38dcf06bb
Naggelus <25223259+ngelus@users.noreply.github.com>
2023-05-27 00:18:37 +0200

    implemented record on speech. only for owner for now for testing. also saving to pcm file.

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,5 +1,14 @@
+const path = require('path');
+const fs = require('fs');
+const ffmpeg = require('ffmpeg');
+const prism = require('prism-media');
 const { Events, VoiceState } = require('discord.js');
-const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
+const {
+  joinVoiceChannel,
+  getVoiceConnection,
+  EndBehaviorType,
+  VoiceReceiver,
+} = require('@discordjs/voice');
 require('dotenv').config();
 const { DISCORD_OWNER_ID: OWNER_ID } = process.env;
 
@@ -8,6 +17,8 @@ module.exports = {
   once: false,
   async execute(oldState, newState) {
     var vc = '';
+    var audioReceiver = '';
+    var audioReceiveStream = '';
     if (oldState.channel == newState.channel) {
       /**
        * When a user deafens or mutes themselfs.
@@ -30,6 +41,39 @@ module.exports = {
           console.error(
             `[ERROR] Error joining ${newState.member.voice.channel?.name}!`
           );
+          console.error(error);
+        }
+        try {
+          if (vc) {
+            audioReceiver = vc.receiver;
+            audioReceiver.speaking.on('start', (uid) => {
+              if (uid == OWNER_ID) {
+                console.log('OWNER IS SPEAKING!');
+                audioReceiveStream = audioReceiver.subscribe(uid, {
+                  end: {
+                    behavior: EndBehaviorType.AfterSilence,
+                    duration: 100,
+                  },
+                });
+
+                const recordingPath =
+                  __dirname + `/../recordings/${Date.now()}.pcm`;
+
+                const writeStream = fs.createWriteStream(recordingPath);
+
+                const opusDecoder = new prism.opus.Decoder({
+                  frameSize: 960,
+                  channels: 2,
+                  rate: 48000,
+                });
+                var streamToFFMPEG = audioReceiveStream
+                  .pipe(opusDecoder)
+                  .pipe(writeStream);
+              }
+            });
+          }
+        } catch (error) {
+          console.error(`[ERROR] Error creating readable!`);
           console.error(error);
         }
         return;
